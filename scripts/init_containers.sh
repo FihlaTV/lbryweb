90d6dbd608fa602ef202e6773c2b3ac7455624bd
Andrey Beletsky <andrey@vr2.net>
2018-11-05 23:47:31 +0700

    Improve containers script (now waits for psql to start accepting connections)

--- scripts/init_containers.sh
+++ scripts/init_containers.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 docker-compose build
+echo "Creating db container"
+docker-compose up -d db
+docker-compose start db
+echo "Waiting for db"
+while ! curl -S localhost:5432 2>&1 | grep 52; do echo "Waiting for db" && sleep 1; done
+echo "Resetting the database"
 docker-compose run app pipenv run python manage.py reset_db --noinput
+echo "Running migrations"
 docker-compose run app pipenv run python manage.py migrate
 docker-compose up
