fade6426f68139982e90bb72bcf47ed7991b253d
Andrey Beletsky <andrey@vr2.net>
2019-01-24 17:47:12 +0700

    Tweak assets paths

--- lbryweb/main/urls.py
+++ lbryweb/main/urls.py
@@ -6,6 +6,6 @@ from . import views
 
 urlpatterns = [
     path('', views.MainView.as_view(), name='main'),
-    path('api_proxy/', csrf_exempt(views.APIProxyView.as_view()), name='api_proxy'),
+    path('api/proxy', csrf_exempt(views.APIProxyView.as_view()), name='api_proxy'),
     path('app/', views.AppView.as_view(), name='app'),
 ]
